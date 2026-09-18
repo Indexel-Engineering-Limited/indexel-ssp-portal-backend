a1c4a963436103d5d2a979e04311d978f4c952b5
indexelrakesh <indexel.rakesh@gmail.com>
2026-09-18 14:19:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://10.10.1.119:5173",
-       "http://61.2.243.13:5173",
+       "https://ssp.indexel.co.in",
       "https://grey-kangaroo-394580.hostingersite.com"
     ],
     credentials: true,
